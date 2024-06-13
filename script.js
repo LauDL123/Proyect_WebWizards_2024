c52b93318267191834c0361a994856a98364b2b9
Lautaro_de_Leon <lautadeleon222@gmail.com>
2024-06-12 22:47:22 -0300

    Programando las bases
    En la siguiente actualización se añadió lo siguiente:
    
    -Todos los archivos .HTML a todos los elementos de la barra de menu.
    
    -Se resolvieron errores.
    
    -Se añadió un carrusel de imágenes.
    
    -Se actualizo el documento de JavaScript y CSS.
    
    -Se añadió un footer con copyright y un link al team Webwizards.
    
    -Se añadió la opción “Iniciar Sesión” al menú de inicio y ya esta totalmente funcionando con su respectivo CSS.
    
    NOTAS QUE REPARAR EN LA SIGUIENTE REVISIÓN:
    
    -Arreglar el carrusel.
    
    -añadir más apartados al índex.
    
    -Terminar de detallar las páginas varias del menú.

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function mostrarFormulario() {
     var formulario = document.getElementById("mensaje_formulario");
     formulario.style.display = "block";
 }
+ // Main carousel
+ $(".carousel .owl-carousel").owlCarousel({
+    autoplay: true,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    items: 1,
+    smartSpeed: 300,
+    dots: false,
+    loop: true,
+    nav : false
+});
