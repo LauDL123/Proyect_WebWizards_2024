b54e851ca65c4afc80838d059252315810ad2e2b
TabareQuintana <tabaquintana@gmail.com>
2024-09-06 19:44:45 -0300

    Slider

--- Body/index.php
+++ Body/index.php
@@ -17,6 +17,7 @@ session_start();
     <script src="https://kit.fontawesome.com/2ff8e04842.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css de practica.css">
     <script src="script.js" defer></script>
+    <script src="../Scripts/slider.js"></script>
 </head>
 <body>
 
@@ -36,7 +37,7 @@ session_start();
     </nav>
 </header>
     <!-- Carousel Start -->
-    <div class="slider-frame">
+    <div class="slider">
         <ul>
             <li><img src="cerrajeria1.JPG" alt=""></li>
             <li><img src="cerradura.jpg" alt=""></li>
